81cd07773e485811137812ca56d787a4480a3fdc
tiana <tiana_l@live.com>
2017-06-08 13:31:10 +0300

    optimization of code

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -3,22 +3,23 @@
 GameBoard::GameBoard(OriginalBoard& originalBoard) : _originalBoard(&originalBoard) {}
 
 char GameBoard::charAt(Coordinate c) const {
-	if (c.row > 0 && c.row <= _originalBoard->rows() &&
-		c.col> 0 && c.col <= _originalBoard->cols() &&
-		c.depth> 0 && c.depth <= _originalBoard->depth())
+	std::map<Coordinate, char>::const_iterator cur = _updatedBoard.find(c);
+	if (cur != _updatedBoard.end())
 	{
-		std::map<Coordinate, char>::const_iterator cur = _updatedBoard.find(c);
-		if (cur != _updatedBoard.end())
-		{
-			return cur->second;
-		}
+		return cur->second;
 	}
 	return _originalBoard->charAt(c);
 }
 
 void GameBoard::setSymbol(Coordinate& attackIndexes, char newSymbol)
 {
-	_updatedBoard[attackIndexes] = newSymbol;
+	if (attackIndexes.row > 0 && attackIndexes.row <= _originalBoard->rows() &&
+		attackIndexes.col> 0 && attackIndexes.col <= _originalBoard->cols() &&
+		attackIndexes.depth> 0 && attackIndexes.depth <= _originalBoard->depth())
+	{
+		_updatedBoard[attackIndexes] = newSymbol;
+
+	}
 }
 
 std::pair<AttackResult, int> GameBoard::checkAttackResult(Coordinate& attackMove) const
